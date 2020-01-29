75d8c27ea644c56c084c72980e35623358807990
Robbie Gay <robbiecgay@gmail.com>
2020-01-29 09:43:36 -0500

    Created Calculator project

---
+++ calculator/calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by Robbie Gay on 1/29/20.
+//  Copyright © 2020 robbiegay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
