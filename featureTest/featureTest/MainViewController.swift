19c62b16ac39b34eb1ed7f78d01ab77d2631a6d0
Robbie Gay <robbiecgay@gmail.com>
2020-02-02 13:34:03 -0500

    Added UILabel, UIButton, UISegmentedControl, UITextField, UITextView, and UISlider to FeatureTest

--- featureTest/featureTest/MainViewController.swift
+++ featureTest/featureTest/MainViewController.swift
@@ -13,8 +13,8 @@ class MainViewController: UIViewController {
     /*
      -Label-
      -Button-
-     Segmented Text
-     Text Field
+     -Segmented Text-
+     -Text Field-
      Slider
      Switch
      Activity Indicator
@@ -22,17 +22,25 @@ class MainViewController: UIViewController {
      Table View
      Table View Cell
      Iamge View
-    */
+     
+     TinyConstraint Stack
+     */
     
     let testLabel = UILabel()
     let testButton = UIButton()
     let testSeg = UISegmentedControl()
-
+    let testTextField = UITextField()
+    let testTextView = UITextView()
+    let testSlider = UISlider()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(testLabel)
         view.addSubview(testButton)
         view.addSubview(testSeg)
+        view.addSubview(testTextField)
+        view.addSubview(testTextView)
+        view.addSubview(testSlider)
         
         styleSubviews()
     }
@@ -52,7 +60,7 @@ class MainViewController: UIViewController {
         testButton.setTitle("This is a UIButton", for: .normal)
         testButton.topToBottom(of: testLabel, offset: 50)
         testButton.leadingToSuperview(offset: 50)
-        testButton.width(300)
+        testButton.centerXToSuperview()
         testButton.layer.borderWidth = 3
         testButton.titleEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
         testButton.layer.cornerRadius = 10
@@ -63,10 +71,58 @@ class MainViewController: UIViewController {
         testSeg.backgroundColor = #colorLiteral(red: 0.8044971824, green: 0.2484204769, blue: 0.2948410213, alpha: 1)
         testSeg.leadingToSuperview(offset: 50)
         testSeg.topToBottom(of: testButton, offset: 50)
+        testSeg.centerXToSuperview()
+        testSeg.insertSegment(withTitle: "Red", at: 0, animated: true)
+        testSeg.insertSegment(withTitle: "Green", at: 1, animated: true)
+        testSeg.insertSegment(withTitle: "Blue", at: 2, animated: true)
+        testSeg.addTarget(self, action: #selector(handleSegClick), for: .valueChanged)
+        
+        testTextField.leadingToSuperview(offset: 50)
+        testTextField.topToBottom(of: testSeg, offset: 50)
+        testTextField.centerXToSuperview()
+//        testTextField.layoutMargins = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        testTextField.height(35)
+        testTextField.layer.borderWidth = 3
+        testTextField.layer.cornerRadius = 10
+        testTextField.layer.masksToBounds = true
+        testTextField.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        testTextField.placeholder = "Type here"
+        testTextField.addTarget(self, action: #selector(handleTextChange), for: .allEvents)
+        
+        testTextView.leadingToSuperview(offset: 50)
+        testTextView.topToBottom(of: testTextField, offset: 50)
+        testTextView.centerXToSuperview()
+        testTextView.layer.borderWidth = 3
+        testTextView.layer.cornerRadius = 10
+        testTextView.layer.masksToBounds = true
+        testTextView.height(150)
+        testTextView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        testSlider.leadingToSuperview(offset:50)
+        testSlider.topToBottom(of: testTextView, offset: 50)
+        testSlider.centerXToSuperview()
+        testSlider.isContinuous = true
+        testSlider.setValue(0.5, animated: true)
+        testSlider.addTarget(self, action: #selector(handleSliderChange), for: .valueChanged)
     }
     
     @objc func handleButtonClick() {
         view.backgroundColor = view.backgroundColor == #colorLiteral(red: 0.8044971824, green: 0.2484204769, blue: 0.2948410213, alpha: 1) ? #colorLiteral(red: 0.9499809146, green: 0.4625762105, blue: 0.1792234778, alpha: 1) : #colorLiteral(red: 0.8044971824, green: 0.2484204769, blue: 0.2948410213, alpha: 1)
         testButton.backgroundColor = view.backgroundColor == #colorLiteral(red: 0.9499809146, green: 0.4625762105, blue: 0.1792234778, alpha: 1) ? #colorLiteral(red: 0.8044971824, green: 0.2484204769, blue: 0.2948410213, alpha: 1) : #colorLiteral(red: 0.9499809146, green: 0.4625762105, blue: 0.1792234778, alpha: 1)
     }
+    
+    @objc func handleSegClick() {
+        let selected = testSeg.selectedSegmentIndex
+        view.backgroundColor = selected == 0 ? #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1) : selected == 1 ? #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1) : #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+    }
+    
+    @objc func handleTextChange() {
+        testTextView.text = testTextField.text!
+    }
+    
+    @objc func handleSliderChange() {
+        let value = testSlider.value
+        view.backgroundColor = UIColor(displayP3Red: 200/255, green: CGFloat((255 * value)/255), blue: CGFloat((255 * value)/255), alpha: 1)
+    }
+    
 }
