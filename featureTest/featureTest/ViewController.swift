8aead1b09575577254feb822d2178ca4eb04bfdc
Robbie Gay <robbiecgay@gmail.com>
2020-02-12 12:48:02 -0500

    Added sections and data to the tabel

--- featureTest/featureTest/ViewController.swift
+++ featureTest/featureTest/ViewController.swift
@@ -10,7 +10,9 @@ import TinyConstraints
 
 class ViewController: UITableViewController {
     
-    let data = ["Robbie", "Anna", "Poppy", "Waffles"]
+    let humans = ["Robbie", "Anna"]
+    let cats = ["Waffles", "Poppy"]
+    
     let cellID = "cellID"
 
     override func viewDidLoad() {
@@ -21,16 +23,58 @@ class ViewController: UITableViewController {
         // This line switches the title from large to small when yo scroll
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        // Names the delegate
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let humansLabel = UILabel()
+        humansLabel.text = "Humans"
+        humansLabel.backgroundColor = .lightGray
+        
+        let catLabel = UILabel()
+        catLabel.text = "Cats"
+        catLabel.backgroundColor = .lightGray
+        
+        if section == 0 {
+            return humansLabel
+        } else {
+            return catLabel
+        }
     }
     
+    // Creates table sections
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    // Creates rows for EACH SECTION (Adds all rows to each section)
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return humans.count
+        } else {
+            return cats.count
+        }
+    }
+    
+    // Creates the cell in EACH ROW (Adds a cell to each row)
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        cell.textLabel?.text = data[indexPath.row]
-        return cell
+        let humanCell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let catCell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let humanName = humans[indexPath.row]
+        humanCell.textLabel?.text = "\(humanName) [Section: \(indexPath.section) Row: \(indexPath.row)]"
+        
+        let catName = cats[indexPath.row]
+        catCell.textLabel?.text = "\(catName) [Section: \(indexPath.section) Row: \(indexPath.row)]"
+        
+        if indexPath.section == 0 {
+            return humanCell
+        } else {
+            return catCell
+
+        }
     }
 }
+
+// You can right click, and select "refactor" to change all the names in the document at once!
