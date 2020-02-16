b6cdec0f71bc56082bff82201a04aa928cbd89c8
Robbie Gay <robbiecgay@gmail.com>
2020-02-15 20:09:21 -0500

    Created Learning Firebase project

---
+++ learning-firebase/learning-firebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learning-firebase
+//
+//  Created by Robbie Gay on 2/15/20.
+//  Copyright © 2020 robbiegay. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
