a4cca9409979f53ceebdfb52e6ffbb0d9f3d858b
Robbie Gay <robbiecgay@gmail.com>
2020-02-16 00:01:38 -0500

    Working on adding coll anoll and docs to database

--- learning-firebase/learning-firebase/AppDelegate.swift
+++ learning-firebase/learning-firebase/AppDelegate.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Configure the Firebase app
+        FirebaseApp.configure()
+        
         return true
     }
 
