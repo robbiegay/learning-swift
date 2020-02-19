3d745cab32ac7bba8f0785941cd437b73d15a6e4
Robbie Gay <robbiecgay@gmail.com>
2020-02-19 13:24:45 -0500

    Initialized Learning watchOS project

---
+++ learning-watch/learning-watch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  learning-watch WatchKit Extension
+//
+//  Created by Robbie Gay on 2/19/20.
+//  Copyright © 2020 robbiegay. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ learning-watch/learning-watch WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  learning-watch WatchKit Extension
+//
+//  Created by Robbie Gay on 2/19/20.
+//  Copyright © 2020 robbiegay. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
