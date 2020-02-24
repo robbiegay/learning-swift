769366cfa5311ecd4ffafb39482af1c7fc804509
Robbie Gay <robbiecgay@gmail.com>
2020-02-24 12:01:46 -0500

    added buttons, alerts, action sheets, and toggle to watch

--- learning-watch/learning-watch WatchKit Extension/ContentView.swift
+++ learning-watch/learning-watch WatchKit Extension/ContentView.swift
@@ -7,15 +7,68 @@
 //
 
 import SwiftUI
+import MapKit
+
+/*
+ ~~alerts and action sheets~~
+ ~~buttons~~ (Button)
+ - dates and timers
+ - groups
+ - images
+ - labels
+ - menus
+ - movies
+ - pickers
+ - sliders
+ ~~switches~~ (Toggle)
+ - tables
+ */
 
 struct ContentView: View {
+    @State private var showGreeting = false
+    @State private var count = 0
+    @State private var showingSheet = false
+    
     var body: some View {
-        Text("Hello, World!")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        ScrollView {
+            VStack {
+                // Action Sheet and Alerts
+                Button(action: {
+                    self.showingSheet = true
+                }) {
+                    Text("Action Sheet")
+                }
+                .actionSheet(isPresented: $showingSheet) {
+                    ActionSheet(title: Text("What do you want to do?"), message: Text("There's only one choice..."), buttons: [
+                        .default(Text("Default")),
+                        .destructive(Text("Destructive")),
+                    ])
+                }
+//                .alert(isPresented: $showingSheet) {
+//                    Alert(title: Text("Important message"), message: Text("Wear sunscreen"), dismissButton: .default(Text("Got it!")))
+//                }
+                
+                
+                // Buttons
+                Button(action: {
+                    self.count += 1
+                }) {
+                    Text("Add 1")
+                }
+                Text("Count: \(count)")
+                    .font(.caption)
+                    .fontWeight(.bold)
+                
+                // Toggle
+                Toggle(isOn: $showGreeting) {
+                    Text("Toggle:")
+                }
+                if showGreeting {
+                    Text("Goodbye")
+                } else {
+                    Text("Hello")
+                }
+            }
+        }
     }
 }
--- learning-watch/learning-watch WatchKit Extension/NotificationView.swift
+++ learning-watch/learning-watch WatchKit Extension/NotificationView.swift
@@ -14,8 +14,8 @@ struct NotificationView: View {
     }
 }
 
-struct NotificationView_Previews: PreviewProvider {
-    static var previews: some View {
-        NotificationView()
-    }
-}
+//struct NotificationView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        NotificationView()
+//    }
+//}
